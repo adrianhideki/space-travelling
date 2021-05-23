f583f9b5013d9a7ebc84a60f91b476e2377652d1
Adrian Hideki <adrian.hideki.br@gmail.com>
2021-05-23 15:38:38 -0300

    Obtendo posts do prismic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
+import Prismic from '@prismicio/client';
 import { GetStaticProps } from 'next';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 
-import { getPrismicClient } from '../services/prismic';
-
-import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import commonStyles from '../styles/common.module.scss';
+import { getPrismicClient } from '../services/prismic';
 
 interface Post {
   uid?: string;
@@ -24,15 +26,45 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): React.ReactElement {
+export default function Home({
+  postsPagination,
+}: HomeProps): React.ReactElement {
   return <>Hellow</>;
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  // const prismic = getPrismicClient();
-  // const postsResponse = await prismic.query(TODO);
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'post')],
+    {
+      fetch: ['post.title', 'post.subtitle', 'post.author'],
+      pageSize: 20,
+      // page: 1,
+    }
+  );
+
+  const posts = postsResponse.results.map(p => ({
+    uid: p.uid,
+    first_publication_date: format(
+      new Date(p.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    ),
+    data: {
+      title: p.data.title,
+      subtitle: p.data.subtitle,
+      author: p.data.author,
+    },
+  }));
+
+  console.log(postsResponse);
 
   return {
-    props: {},
+    props: {
+      next_page: postsResponse.next_page,
+      results: posts,
+    },
   };
 };
